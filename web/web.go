682f500281b0951f375a336abd12b7f560104e01
wlfvpn <vpn.womanlifefreedom.vip>
2023-01-26 00:37:38 +0000

    removed ip recording

--- web/web.go
+++ web/web.go
@@ -311,7 +311,7 @@ func (s *Server) startTask() {
 	s.cron.AddJob("@every 30s", job.NewCheckInboundJob())
 
 	// check client ips from log file every 10 sec
-	s.cron.AddJob("@every 10s", job.NewCheckClientIpJob())
+	// s.cron.AddJob("@every 10s", job.NewCheckClientIpJob())
 
 	// 每一天提示一次流量情况,上海时间8点30
 	var entry cron.EntryID
